935dbf29e9235c4a5bdeec027c68909d65e2473b
Marcos Brito <marcosbrito@Marcoss-MacBook-Pro.local>
2021-01-20 09:50:07 +0000

    second commit

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ function calculateAll() {
 
       let totalvalue = document.querySelector('h2 span');
       totalvalue.innerText = total;
-
+    }
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
   // const singleProduct = document.querySelector('.product');
@@ -33,14 +33,14 @@ function calculateAll() {
  
 
   // ITERATION 3
-    }
+    
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  
 }
 
 // ITERATION 5
